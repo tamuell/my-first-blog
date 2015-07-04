a0a7be20dd04e58ccddb7df9ba76560076658f24
tamuell <tatiana.mueller@live.de>
2015-07-04 12:10:24 +0200

    My Django Girls app, first commit

---
+++ Testdatei.py
@@ -0,0 +1,26 @@
+name = "Tatiana"
+print(name)
+
+if 3 > 2: 
+	print("It works!")
+if 5 > 2:
+	print("5 is indeed greater than 2")
+else:
+	print("5 is not greater than 2")
+	name = 'Tatiana'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Tatiana':
+    print('Hey Tatiana!')
+else:
+    print('Hey anonymous!')
+
+def hi():
+	print('Hi there!')
+	print('How are you?')
+hi ()
+def hi(name):
+    print('Hi ' + name + '!')
+
+hi("Tatiana")
+
